504bcdf5e296b25d0d8c0619645d931eeb2e869d
Bartosz Paternoga <37982247+bartosz-paternoga@users.noreply.github.com>
2018-07-22 14:59:50 +0200

    Add files via upload

--- App.js
+++ App.js
@@ -9,126 +9,94 @@ class App extends Component {
 
      startWebcam = () =>{
 
-     
       
-      
-      navigator.getUserMedia (
+        navigator.getUserMedia (
 
-        // constraints
-              {
-                 video: true,
-                 audio: false
-              },
+          // constraints
+                {
+                   video: true,
+                   audio: false
+                },
 
-        // successCallback
-              function(a) {   
-              const video = document.querySelector('video');
+          // successCallback
+                function(a) {   
+                const video = document.querySelector('video');
 
-              video.srcObject = a; 
+                video.srcObject = a; 
 
-              },
+                },
 
-        // errorCallback
-              function() {}
+          // errorCallback
+                function() {}
 
-      );
-
+        );
 
     } 
 
 
+    main = async () => {
 
+      this.startWebcam();
 
-main = async () => {
+      var canvas = document.getElementById('my-canvas');
+      var ctx = canvas.getContext('2d');
+      var img = document.querySelector('video');
 
-
-
-this.startWebcam();
-
-
-  var canvas = document.getElementById('my-canvas');
-  var ctx = canvas.getContext('2d');
-  var img = document.querySelector('video');
-
-
-    canvas.setAttribute('width', `${img.width}`);
-    canvas.setAttribute('height', `${img.height}`);
-
-
-
+      canvas.setAttribute('width', `${img.width}`);
+      canvas.setAttribute('height', `${img.height}`);
 
       var imageScaleFactor = 0.5;
       var outputStride = 16;
       var flipHorizontal = false;
 
-
-  
+      
       var model = await posenet.load();
+
 
       while (true) {
 
-      let poses = [];
+        let poses = [];
 
-      const pose = await model.estimateSinglePose(img, imageScaleFactor, flipHorizontal, outputStride)
 
-      poses.push(pose);
+         const pose = await model.estimateSinglePose(img, imageScaleFactor, flipHorizontal, outputStride);
+
+
+        const modelLoad = "LOADED";
+
+        if (modelLoad !=="") {
+          const elem1 = document.getElementById('loading-message');
+          elem1.style.display = 'none';
+          const elem2 = document.getElementById('sk-cube-grid');
+          elem2.style.display = 'none';
+        }
+
+
+         poses.push(pose);
+
+         ctx.drawImage(img, 0, 0, 640, 480);
+
+          pose.keypoints.forEach(function(keypoint) {
+              if (keypoint.score > 0.9) {
+
+              ctx.beginPath();
+              ctx.fillStyle = 'rgb(255, 255, 89)';
+              ctx.fillText(keypoint.part, keypoint.position.x + 10, keypoint.position.y);
+              ctx.arc(keypoint.position.x, keypoint.position.y, 5, 0, Math.PI * 2, false);
+              ctx.fillStyle = 'rgb(255, 0, 0)';
+              ctx.fill();
+
+              console.log("/body part:", keypoint.part, "/score:", keypoint.score, 
+                "/position.x:", keypoint.position.x, "/position.y:", keypoint.position.y);
+
+                }
+            });
+
+            await tf.nextFrame();
+
+          }
+
+        }
         
-     // console.log(poses);
-
-
-
-
-ctx.drawImage(img, 0, 0, 640, 480);
-
-pose.keypoints.forEach(function(keypoint) {
-          if (keypoint.score > 0.9) {
-
-
-
-          ctx.beginPath();
-          ctx.fillStyle = 'rgb(255, 255, 89)';
-          ctx.fillText(keypoint.part, keypoint.position.x + 10, keypoint.position.y);
-          ctx.arc(keypoint.position.x, keypoint.position.y, 5, 0, Math.PI * 2, false);
-          ctx.fillStyle = 'rgb(255, 0, 0)';
-          ctx.fill();
-
-
-
-console.log(keypoint.part, keypoint.score, keypoint.position.x, keypoint.position.y);
-
-
-            }
-        });
-
-        await tf.nextFrame();
-
-
-      }
-
-    }
-    
-
-
-
-    
-
-
-
-
-
-      
-
-    
-
-
-
-
-          
-
-
-
-
-
 
 
    render() {
--- component1.js
+++ component1.js
@@ -1,23 +1,44 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-
 
 const Comp = (props) => (
 
  
 <div >
-	
-	<button onClick={props.main}>testo</button>
+
+
+    <div className="App" onLoad = {props.main}>
+      <header className="App-header" >
+        <img src={logo} className="App-logo" alt="logo" /> 
+        <h4 className="App-title">PoseNet with Tensorflow.js and REACT</h4>
+      </header>     
+    </div> <br/>
+
+    <div id="loading-message">
+       <p>PoseNet model is loading. This will take a few moments ...</p>
+       <p>All goods things come for those who wait</p>
+    </div>
+    
+	<div className="sk-cube-grid" id="sk-cube-grid">
+	  <div className="sk-cube sk-cube1"></div>
+	  <div className="sk-cube sk-cube2"></div>
+	  <div className="sk-cube sk-cube3"></div>
+	  <div className="sk-cube sk-cube4"></div>
+	  <div className="sk-cube sk-cube5"></div>
+	  <div className="sk-cube sk-cube6"></div>
+	  <div className="sk-cube sk-cube7"></div>
+	  <div className="sk-cube sk-cube8"></div>
+	  <div className="sk-cube sk-cube9"></div>
+	</div>
+
+
+
 
     <div id="canvas-wrapper" >
         <canvas id="my-canvas"></canvas>
  		<video id="video"  width="640" height="480" controls autoPlay ></video>
     </div>
-
-
-
-
-
 
 
 
