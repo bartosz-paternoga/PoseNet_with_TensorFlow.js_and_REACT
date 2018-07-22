6ad0642392d9cbb78a258d0e4d382294ee613eb3
Bartosz Paternoga <37982247+bartosz-paternoga@users.noreply.github.com>
2018-07-22 17:25:28 +0200

    Add files via upload

--- App.js
+++ App.js
@@ -76,7 +76,7 @@ class App extends Component {
          ctx.drawImage(img, 0, 0, 640, 480);
 
           pose.keypoints.forEach(function(keypoint) {
-              if (keypoint.score > 0.9) {
+              if (keypoint.score > 0.50) {
 
               ctx.beginPath();
               ctx.fillStyle = 'rgb(255, 255, 89)';
@@ -85,17 +85,57 @@ class App extends Component {
               ctx.fillStyle = 'rgb(255, 0, 0)';
               ctx.fill();
 
-              console.log("/body part:", keypoint.part, "/score:", keypoint.score, 
-                "/position.x:", keypoint.position.x, "/position.y:", keypoint.position.y);
+            //  console.log("/body part:", keypoint.part, "/score:", keypoint.score, 
+              //  "/position.x:", keypoint.position.x, "/position.y:", keypoint.position.y);
 
                 }
             });
 
-            await tf.nextFrame();
 
+
+
+      const adjacentKeyPoints = posenet.getAdjacentKeyPoints(
+            pose.keypoints, 0.1);
+
+            adjacentKeyPoints.forEach((keypoints) => {
+                  console.log(keypoints);
+            });
+          
+
+      function toTuple({y, x}) {
+              return [y, x];
+            }
+
+
+       function drawSkeleton(keypoints, minConfidence , ctx, scale = 1) {
+          const adjacentKeyPoints = posenet.getAdjacentKeyPoints(
+                 keypoints, minConfidence);
+
+                adjacentKeyPoints.forEach((keypoints) => {
+                  drawSegment(toTuple(keypoints[0].position),
+                  toTuple(keypoints[1].position), 'aqua', scale, ctx);
+              });
+            }  
+
+
+       function drawSegment([ay, ax], [by, bx], color, scale, ctx) {
+            ctx.beginPath();
+            ctx.moveTo(ax * scale, ay * scale);
+            ctx.lineTo(bx * scale, by * scale);
+            ctx.lineWidth = 3;
+            ctx.strokeStyle = 'aqua';
+            ctx.stroke();
           }
 
-        }
+
+        drawSkeleton(pose.keypoints, 0.5, ctx);
+
+
+       await tf.nextFrame();
+
+      }
+
+    }
         
 
 
